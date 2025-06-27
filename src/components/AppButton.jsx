d4b6c90e379c54420a871003d2daa458978f5d18
ko357255 <mikanace@outlook.jp>
2025-06-27 09:18:42 +0900

    AppButtonコンポーネントをLinkに対応させ、CSSスタイルを削除

--- src/components/AppButton.jsx
+++ src/components/AppButton.jsx
@@ -1,22 +1,34 @@
 import './AppButton.css';
+import { Link } from 'react-router-dom';
 
 export const AppButton = ({
   children, // 中の子要素がここに入る
+  to, // Linkの場合は必須
   type = 'button',
   className = '',
   disable = false,
   onClick,
   ...props // 任意でさらに引数を取る
 }) => {
-  return (
-    <button
-      type={type}
-      className={`app_button ${className}`}
-      disabled={disable}
-      onClick={onClick}
-      {...props} // propsを展開する
-    >
-      {children}
-    </button>
-  );
+  const buttonClassName = `app_button ${className}`;
+
+  if (to) { // Link
+    return (
+      <Link to={to} className={buttonClassName} {...props}>
+        {children}
+      </Link>
+    );
+  } else { // button
+    return (
+      <button
+        type={type}
+        className={buttonClassName}
+        disabled={disable}
+        onClick={onClick}
+        {...props} // propsを展開する
+      >
+        {children}
+      </button>
+    );
+  }
 };
