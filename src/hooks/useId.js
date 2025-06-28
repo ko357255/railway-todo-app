256cd361bd2fb3db310194a3450dcd41e6250282
ko357255 <mikanace@outlook.jp>
2025-06-28 12:25:38 +0900

    コメントアウト

--- src/hooks/useId.js
+++ src/hooks/useId.js
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 
+// ランダムな値を返すフック
 export const useId = () => {
+  // useState()を使うことで、再レンダリングされても値が変わらない
   const [id] = useState(() => {
     return Math.random().toString(32).substring(2);
   });
