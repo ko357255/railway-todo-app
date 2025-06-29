31d3071465412f241fea1f46c3ff6a0b3ce02951
ko357255 <mikanace@outlook.jp>
2025-06-29 13:00:14 +0900

    タスク編集をモーダル化

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -8,7 +8,8 @@ export const Modal = ({ isOpen, onClose, children }) => {
   return (
     // フォーカスを閉じ込める
     <FocusLock returnFocus>
-      {/* クリック時に渡された onClose を実行する */}
+      {/* 黒背景 */}
+      {/* クリック時に渡された onClose を実行し、isOpenをfalseにする */}
       <div className="modal_overlay" onClick={onClose}>
         {/* モーダル内部はクリックイベントを無効化する */}
         <div className="modal_content" onClick={(e) => e.stopPropagation()}>
--- src/components/TaskEditModal.jsx
+++ src/components/TaskEditModal.jsx
@@ -1,6 +1,6 @@
 import { useCallback, useState, useEffect } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 import { parseISO } from 'date-fns';
 import { fromZonedTime, format } from 'date-fns-tz';
 import { ModalBackButton } from '~/components/ModalBackButton';
@@ -14,9 +14,8 @@ import { useId } from '~/hooks/useId';
 
 export const TaskEditModal = ({ task, isOpen, onClose }) => {
   const id = useId();
-
-  const { listId, taskId } = useParams();
-  const navigate = useNavigate();
+  const { listId } = useParams();
+  const taskId = task.id;
   const dispatch = useDispatch();
 
   const [title, setTitle] = useState('');
@@ -24,20 +23,23 @@ export const TaskEditModal = ({ task, isOpen, onClose }) => {
   const [done, setDone] = useState(false);
   const [limitDate, setLimitDate] = useState('');
   const [limitTime, setLimitTime] = useState('');
-
   const [errorMessage, setErrorMessage] = useState('');
   const [isSubmitting, setIsSubmitting] = useState(false);
 
+  // タスクの初期値をセット
   useEffect(() => {
     if (task) {
+      setErrorMessage('');
       setTitle(task.title);
       setDetail(task.detail);
       setDone(task.done);
-
       if (task.limit) {
         const utcDate = parseISO(task.limit);
         setLimitDate(format(utcDate, 'yyyy-MM-dd', { timeZone: 'Asia/Tokyo' }));
         setLimitTime(format(utcDate, 'HH:mm', { timeZone: 'Asia/Tokyo' }));
+      } else {
+        setLimitDate('');
+        setLimitTime('');
       }
     }
   }, [task]);
@@ -45,7 +47,7 @@ export const TaskEditModal = ({ task, isOpen, onClose }) => {
   useEffect(() => {
     void dispatch(setCurrentList(listId));
     void dispatch(fetchTasks());
-  }, [listId]);
+  }, [listId, dispatch]); // dispatchを含めるべきらしい
 
   const onSubmit = useCallback(
     (event) => {
@@ -54,7 +56,6 @@ export const TaskEditModal = ({ task, isOpen, onClose }) => {
       setIsSubmitting(true);
 
       const taskPayload = { id: taskId, title, detail, done, limit: null };
-
       if (limitDate && limitTime) {
         const utcLimit = fromZonedTime(
           `${limitDate}T${limitTime}:00`,
@@ -63,10 +64,10 @@ export const TaskEditModal = ({ task, isOpen, onClose }) => {
         taskPayload.limit = utcLimit.toISOString();
       }
 
-      void dispatch(updateTask(taskPayload))
+      dispatch(updateTask(taskPayload))
         .unwrap()
         .then(() => {
-          navigate(`/lists/${listId}`);
+          onClose();
         })
         .catch((err) => {
           setErrorMessage(err.message);
@@ -75,9 +76,10 @@ export const TaskEditModal = ({ task, isOpen, onClose }) => {
           setIsSubmitting(false);
         });
     },
-    [title, taskId, listId, detail, done, limitDate, limitTime],
+    [title, taskId, detail, done, limitDate, limitTime, dispatch, onClose],
   );
 
+  // 削除処理
   const handleDelete = useCallback(() => {
     if (!window.confirm('Are you sure you want to delete this task?')) {
       return;
@@ -96,15 +98,14 @@ export const TaskEditModal = ({ task, isOpen, onClose }) => {
       .finally(() => {
         setIsSubmitting(false);
       });
-  }, [taskId]);
+  }, [taskId, dispatch, onClose]);
 
   if (!isOpen) return null;
 
   return (
-    // 黒背景
     <Modal isOpen={isOpen} onClose={onClose}>
       <div className="edit_task">
-        <ModalBackButton onClick={onClose}/>
+        <ModalBackButton onClick={onClose} />
         <h2 className="edit_task__title">Edit Task</h2>
         <p className="edit_task__error">{errorMessage}</p>
         <form className="edit_task__form" onSubmit={onSubmit}>
@@ -117,6 +118,7 @@ export const TaskEditModal = ({ task, isOpen, onClose }) => {
               placeholder="Buy some milk"
               value={title}
               onChange={(event) => setTitle(event.target.value)}
+              data-autofocus
             />
           </fieldset>
           <fieldset className="edit_task__form_field">
@@ -127,23 +129,27 @@ export const TaskEditModal = ({ task, isOpen, onClose }) => {
               type="textarea"
               id={`${id}-detail`}
               placeholder="Blah blah blah"
+              rows="3"
               value={detail}
               onChange={(event) => setDetail(event.target.value)}
             />
           </fieldset>
           <fieldset aria-labelledby={`${id}-limit`}>
-            <div id={`${id}-limit`} className="edit_task__form_label">
+            <label
+              htmlFor={`${id}-limit-date`}
+              className="edit_task__form_label"
+            >
               limit
-            </div>
+            </label>
             <div className="edit_task__form_date">
               <input
-                id={`${id}-limit`}
+                id={`${id}-limit-date`}
                 type="date"
                 value={limitDate}
                 onChange={(e) => setLimitDate(e.target.value)}
               />
               <input
-                id={`${id}-limit`}
+                id={`${id}-limit-time`}
                 type="time"
                 value={limitTime}
                 onChange={(e) => setLimitTime(e.target.value)}
@@ -176,7 +182,7 @@ export const TaskEditModal = ({ task, isOpen, onClose }) => {
             >
               Delete
             </AppButton>
-            <AppButton type="submit" disabled={isSubmitting}>
+            <AppButton type="submit" disabled={title == '' || detail == '' || isSubmitting}>
               Update
             </AppButton>
           </div>
